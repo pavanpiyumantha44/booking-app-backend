335c4772f18c91ee7da85675ae6bc4aed5ffb502
Pavan Piyumantha <106144514+pavanpiyumantha44@users.noreply.github.com>
2024-10-16 17:00:27 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,6 @@ import User from '../models/User.js';
 const verifyUser = async(req,res,next)=>{
     try {
         const token = req.headers.authorization.split(' ')[1];
-        console.log(token);
         if(!token){
             return res.status(404).json({success:false,error:"Token Not Provided"})
         }
