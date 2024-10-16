c2312b2d2d0a4b1423501a7707b79f17631a5279
pavanpiyumantha44 <pavanpiyumantha222@gmail.com>
2024-10-16 16:08:16 +0530

    Enhanced the server log

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,20 +5,24 @@ const verifyUser = async(req,res,next)=>{
     try {
         const token = req.headers.authorization.split(' ')[1];
         if(!token){
+            console.log("Token not Provided");
             return res.status(404).json({success:false,error:"Token Not Provided"})
         }
         const decoded = jwt.verify(token,process.env.JWT_KEY);
         if(!decoded){
+            console.log("Token not Valid");
             return res.status(404).json({success:false,error:"Token Not Valid"})
         }
         const user = await User.findById({_id:decoded._id}).select('-password')
 
         if(!user){
+            console.log("User Not Found");
             return res.status(404).json({success:false,error:"User Not Found"})
         }
         req.user = user
         next()
     } catch (error) {
+        console.log("Server Error");
         return res.status(500).json({success:false,error:"Server Error"})
     }
 }
